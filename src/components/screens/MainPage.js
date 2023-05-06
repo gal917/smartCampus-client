7ce1ed599ee037f50dae9654e4e3d6da90fa6185
snir95 <83129206+snir95@users.noreply.github.com>
2023-05-06 20:43:56 +0300

    .

--- src/components/screens/MainPage.js
+++ src/components/screens/MainPage.js
@@ -5,12 +5,14 @@ import MainBody from '../MainBody'
 import IFrameTemplate from '../IFrameTemplate'
 
 const MainPage = () => {
+
+  const aaa = "https://app.hittest.smarty.camp/dashboard/8ad04d20-b533-11ed-aa55-4bc57f85b6ff?publicId=25d84fd0-5912-11ed-804e-411dde1247cf"
   return (
     <MainContainer>
       <MainBody/> 
         <h2>GIS Mapping of Hit College</h2>
         <IFrameTemplate title="Mapping of Hit College" src="http://16.170.213.98:80" width="600" height="450" loading="lazy"/>
-        <IFrameTemplate title="Mapping of Hit College" src="https://app.hittest.smarty.camp/dashboard/8ad04d20-b533-11ed-aa55-4bc57f85b6ff?publicId=25d84fd0-5912-11ed-804e-411dde1247cf" width="600" height="450" loading="lazy"/>
+        <IFrameTemplate title="Mapping of Hit College" src={aaa} width="600" height="450" loading="lazy"/>
     </MainContainer>
   )
   

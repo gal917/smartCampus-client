079a31baf7191355a05920ea4c3ef17e018bf21e
snir95 <83129206+snir95@users.noreply.github.com>
2023-05-06 19:38:59 +0300

    iframe component

--- src/components/screens/MainPage.js
+++ src/components/screens/MainPage.js
@@ -2,15 +2,18 @@ import React from 'react'
 import "../../Style.css"
 import styled from 'styled-components'
 import MainBody from '../MainBody'
+import IFrameTemplate from '../IFrameTemplate'
 
 const MainPage = () => {
   return (
     <MainContainer>
       <MainBody/> 
         <h2>GIS Mapping of Hit College</h2>
-        <iframe title="Mapping of Hit College" src="http://16.170.213.98:80" width="600" height="450" allowFullScreen="" loading="lazy"></iframe>
+        <IFrameTemplate title="Mapping of Hit College" src="http://16.170.213.98:80" width="600" height="450" loading="lazy"/>
+        <IFrameTemplate title="Mapping of Hit College" src="https://app.hittest.smarty.camp/dashboard/8ad04d20-b533-11ed-aa55-4bc57f85b6ff?publicId=25d84fd0-5912-11ed-804e-411dde1247cf" width="600" height="450" loading="lazy"/>
     </MainContainer>
   )
+  
 }
 
 export default MainPage
